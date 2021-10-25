ec9d53d32bc6f62fb81302192d8bb0a87cea5248
Marc <chlee1252@gmail.com>
2021-10-25 21:26:29 +0900

    fix: fix health check uri

--- src/main/java/com/chlee1252/scheduledmap/transport/controller/HealthCheckController.java
+++ src/main/java/com/chlee1252/scheduledmap/transport/controller/HealthCheckController.java
@@ -1,9 +1,11 @@
 package com.chlee1252.scheduledmap.transport.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/v1/api")
+@RestController
+@RequestMapping(path = "/api/v1")
 public class HealthCheckController {
 
     @GetMapping(value = "/health")
